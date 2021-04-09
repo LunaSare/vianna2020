33656b4e9b99bb3604427c93a0f88b6e3ebc8aab
Luna Sare <sanchez.reyes.luna@gmail.com>
2021-04-09 15:27:53 -0700

    code to replace tips and write tree file

--- R/rename-mitogenome-calibrated-tree.R
+++ R/rename-mitogenome-calibrated-tree.R
@@ -6,9 +6,9 @@ download.file("https://datadryad.org/stash/downloads/file_stream/690496", "data-
 
 phy <- ape::read.nexus("data-raw/mitogenome_calibrated.tree")
 
-# Writing the tip labels down to a file
+# Writing the tip labels and matching new names down to a file
 
-c(phy$tip.label[1:2],
+new_tip_labels <- c(phy$tip.label[1:2],
   "Eudyptes_schlegeli", #3
   "Eudyptes_chrysolophus", #4
   "Eudyptes_chrysolophus", #5
@@ -26,10 +26,45 @@ c(phy$tip.label[1:2],
   "Spheniscus_mendiculus", #18
   "Eudyptula_minor", #19
   "Eudyptula_minor", #20
-  lapply(strsplit(phy$tip.label[21], "_"), "[", -1)
+  "Pygoscelis_papua", #21
+  "Pygoscelis_papua", #22
+  "Pygoscelis_papua", #23
+  "Pygoscelis_papua", #24
+  "Pygoscelis_papua", #25
+  "Pygoscelis_antarcticus", #26
+  "Pygoscelis_antarcticus", #27
+  "Pygoscelis_adeliae", #28
+  "Pygoscelis_adeliae", #29
+  phy$tip.label[30], #30
+  "Aptenodytes_forsteri", #31
+  "Aptenodytes_patagonicus", #32
+  phy$tip.label[33]  #33
   )
 
-paste0(phy$tip.label, ",\t\n")
+csv <- paste0(c("original", phy$tip.label), ",", c("renamed", new_tip_labels))
+
+write(csv, file = "data/tip_labels.csv")
+
+# Reading the file in
+
+tips <- read.csv(file="data/tip_labels.csv", header = TRUE)
+
+head(tips)
+
+# Renaming the tips
+
+## Matching the tip labels to column of original tip names
+ii <- match(phy$tip.label, tips$original)
+
+## Replacing the original tips for the new tips
+
+phy$tip.label <- tips$renamed[ii]
+
+# Writing the tree file down
+
+ape::write.tree(phy, file = "data/mitogenome_calibrated_renamed.tree")
+
+
 
 
 
